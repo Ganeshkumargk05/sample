c2374349dcca21391230d46cb548338c49055815
Ganeshkumar1956 <ganeshkumar1956@gmail.com>
2025-03-03 00:02:20 +0530

    Solution in java

---
+++ solution.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class solution{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        sc.nextLine();
+        String s=sc.nextLine();
+        System.out.println(s+" "+num);   
+    }
+}
